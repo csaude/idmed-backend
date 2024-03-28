55af70d980aeec70d96c47835330f858730bd8e3
colaconhongo <colaco.nhango@fgh.org.mz>
2024-03-28 10:37:29 +0200

    Grails Upgrade From 4.0.1 to Version 6.1.2

--- src/test/groovy/mz/org/fgh/sifmoz/backend/stockadjustment/StockAdjustmentSpec.groovy
+++ src/test/groovy/mz/org/fgh/sifmoz/backend/stockadjustment/StockAdjustmentSpec.groovy
@@ -1,13 +1,19 @@
 package mz.org.fgh.sifmoz.backend.stockadjustment
 
 import grails.testing.gorm.DomainUnitTest
+import mz.org.fgh.sifmoz.backend.protection.Menu
 import spock.lang.Specification
 
 class StockAdjustmentSpec extends Specification implements DomainUnitTest<StockAdjustment> {
 
      void "test domain constraints"() {
         when:
-        StockAdjustment domain = new StockAdjustment()
+        StockAdjustment domain = new StockAdjustment() {
+            @Override
+            List<Menu> hasMenus() {
+                return null
+            }
+        }
         //TODO: Set domain props here
 
         then:
