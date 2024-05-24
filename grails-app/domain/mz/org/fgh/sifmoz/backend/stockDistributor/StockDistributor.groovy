7e74a5f80ff3b1fc6eb47a2e28c70c81fafd0c01
miliate <erciliomiliate@gmail.com>
2024-05-24 10:02:35 +0200

    Stock distribution

---
+++ grails-app/domain/mz/org/fgh/sifmoz/backend/stockDistributor/StockDistributor.groovy
@@ -0,0 +1,42 @@
+package mz.org.fgh.sifmoz.backend.stockDistributor
+
+import mz.org.fgh.sifmoz.backend.base.BaseEntity
+import mz.org.fgh.sifmoz.backend.clinic.Clinic
+import mz.org.fgh.sifmoz.backend.drugDistributor.DrugDistributor
+import mz.org.fgh.sifmoz.backend.protection.Menu
+import mz.org.fgh.sifmoz.backend.stockDistributorBatch.StockDistributorBatch
+
+class StockDistributor extends BaseEntity {
+    String id
+    String orderNumber
+    Date creationDate = new Date()
+    String notes
+    Clinic clinic
+    String status // C- Confirmado, R-Rejeitado, P -Pendente
+
+    static hasMany = [drugDistributors: DrugDistributor]
+
+
+
+    static mapping = {
+        id generator: "assigned"
+        id column: 'id', index: 'Pk_Stock_Batch_Idx'
+    }
+
+    def beforeInsert() {
+        if (!id) {
+            id = UUID.randomUUID()
+        }
+
+    }
+
+
+    @Override
+    List<Menu> hasMenus() {
+        List<Menu> menus = new ArrayList<>()
+        Menu.withTransaction {
+            menus = Menu.findAllByCodeInList(Arrays.asList(patientMenuCode, groupsMenuCode, dashboardMenuCode, stockMenuCode, homeMenuCode))
+        }
+        return menus
+    }
+}
