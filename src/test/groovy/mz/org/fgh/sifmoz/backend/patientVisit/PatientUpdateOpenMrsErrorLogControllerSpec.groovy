707ee0b2c11fbd0c98a4bb68501773b28c399bc1
muhammadchona <muhammadchona@gmail.com>
2025-03-05 22:39:38 +0200

    Adding validation and report to patientUpateFromSesp and bug fix close other services and other episodes.

--- src/test/groovy/mz/org/fgh/sifmoz/backend/patientVisit/PatientUpdateOpenMrsErrorLogControllerSpec.groovy
+++ src/test/groovy/mz/org/fgh/sifmoz/backend/patientVisit/PatientUpdateOpenMrsErrorLogControllerSpec.groovy
@@ -1,6 +1,7 @@
 package mz.org.fgh.sifmoz.backend.patientVisit
 
 import grails.testing.web.controllers.ControllerUnitTest
+import mz.org.fgh.sifmoz.backend.patientUpdateOpenMrsErrorLog.PatientUpdateOpenMrsErrorLogController
 import spock.lang.Specification
 
 class PatientUpdateOpenMrsErrorLogControllerSpec extends Specification implements ControllerUnitTest<PatientUpdateOpenMrsErrorLogController> {
