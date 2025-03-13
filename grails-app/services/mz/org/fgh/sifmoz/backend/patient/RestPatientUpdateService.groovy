4e8ea280b2c996f61413e998a07bf56fb567ee9a
muhammadchona <muhammadchona@gmail.com>
2025-03-13 17:43:13 +0200

    Bug fix , changing to OPENMRS_USER_PROVIDER_UUID

--- grails-app/services/mz/org/fgh/sifmoz/backend/patient/RestPatientUpdateService.groovy
+++ grails-app/services/mz/org/fgh/sifmoz/backend/patient/RestPatientUpdateService.groovy
@@ -47,13 +47,13 @@ class RestPatientUpdateService {
              HealthInformationSystem his = HealthInformationSystem.findWhere(abbreviation: 'OpenMRS')
             if (!his.interoperabilityAttributes.isEmpty()) {
              //   println "Iniciando a Rotina de Busca de Pacientes para Actualizacao"
-                String universalProviderUUid = his.interoperabilityAttributes.find { it.interoperabilityType.code == "UNIVERSAL_PROVIDER_UUID" }.value
+                String universalUserProviderUUid = his.interoperabilityAttributes.find { it.interoperabilityType.code == "OPENMRS_USER_PROVIDER_UUID" }.value
                 String urlBase = his.interoperabilityAttributes.find { it.interoperabilityType.code == "URL_BASE" }.value
 
                 try {
                    // RestOpenMRSClient restPost = new RestOpenMRSClient()
                     String urlPath = "patient/info/updated-data?client_name=iDMED"
-                    def response =  RestOpenMRSClient.getResponseOpenMRSClient(universalProviderUUid, null, urlBase ,urlPath, requestMethod_GET)
+                    def response =  RestOpenMRSClient.getResponseOpenMRSClient(universalUserProviderUUid, null, urlBase ,urlPath, requestMethod_GET)
 
                     if (response?.entry) {
                         response.entry.each { patient ->
