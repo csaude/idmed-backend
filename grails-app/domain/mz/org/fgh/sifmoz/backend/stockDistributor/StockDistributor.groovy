724edcfe2b11645597c4dc2c54ab75cad052eba8
muhammadchona <muhammadchona@gmail.com>
2025-05-21 09:00:15 +0200

    adding new RequestMap structure and roles previliegies

--- grails-app/domain/mz/org/fgh/sifmoz/backend/stockDistributor/StockDistributor.groovy
+++ grails-app/domain/mz/org/fgh/sifmoz/backend/stockDistributor/StockDistributor.groovy
@@ -35,7 +35,7 @@ class StockDistributor extends BaseEntity {
     List<Menu> hasMenus() {
         List<Menu> menus = new ArrayList<>()
         Menu.withTransaction {
-            menus = Menu.findAllByCodeInList(Arrays.asList(patientMenuCode, groupsMenuCode, dashboardMenuCode, stockMenuCode, homeMenuCode))
+            menus = Menu.findAllByCodeInList(Arrays.asList(stockMenuCode,homeMenuCode))
         }
         return menus
     }
---
+++ src/test/groovy/mz/org/fgh/sifmoz/backend/uiSection/UiSectionSpec.groovy
@@ -0,0 +1,18 @@
+package mz.org.fgh.sifmoz.backend.uiSection
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class UiSectionSpec extends Specification implements DomainUnitTest<UiSection> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
